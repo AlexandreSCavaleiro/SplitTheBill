4fe7f86e23ec56fc8da818849e3980f838d75cb9
AlexandreSCavaleiro <alexandre_cavaleiro@outlook.com>
2023-10-30 20:47:25 -0300

    build: adição da ParticipanteActivity.

--- app/src/main/java/br/edu/scl/ifsp/ads/splitthebill/view/ParticipanteActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/splitthebill/view/ParticipanteActivity.kt
@@ -1,4 +1,21 @@
 package br.edu.scl.ifsp.ads.splitthebill.view
 
-class ParticipanteActivity {
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import br.edu.scl.ifsp.ads.splitthebill.databinding.ActivityParticipanteBinding
+
+
+class ParticipanteActivity: AppCompatActivity() {
+    private val apb: ActivityParticipanteBinding by lazy {
+        ActivityParticipanteBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(apb.root)
+
+        setSupportActionBar(apb.toolbarIn.toolbar)
+
+    }
+
 }
