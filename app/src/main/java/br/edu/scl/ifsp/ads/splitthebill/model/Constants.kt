6a840712d69d20615190cf5e0119885c75f3b938
AlexandreSCavaleiro <alexandre_cavaleiro@outlook.com>
2023-10-30 22:42:52 -0300

    build: botão "+" da raise na intent participanteActivity

---
+++ app/src/main/java/br/edu/scl/ifsp/ads/splitthebill/model/Constants.kt
@@ -0,0 +1,5 @@
+package br.edu.scl.ifsp.ads.splitthebill.model
+
+object Constants {
+    const val PARTICIPANTE_EXTRA = "PARTICIPANTE_EXTRA"
+}
--- app/src/main/java/br/edu/scl/ifsp/ads/splitthebill/view/MainActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/splitthebill/view/MainActivity.kt
@@ -1,12 +1,16 @@
 package br.edu.scl.ifsp.ads.splitthebill.view
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.ArrayAdapter
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import br.edu.scl.ifsp.ads.splitthebill.R
 import br.edu.scl.ifsp.ads.splitthebill.databinding.ActivityMainBinding
+import br.edu.scl.ifsp.ads.splitthebill.model.Constants.PARTICIPANTE_EXTRA
 import br.edu.scl.ifsp.ads.splitthebill.model.Participante
 
 class MainActivity : AppCompatActivity() {
@@ -14,6 +18,9 @@ class MainActivity : AppCompatActivity() {
     private val amb: ActivityMainBinding by lazy {
         ActivityMainBinding.inflate(layoutInflater)
     }
+
+    //ARL
+    private lateinit var carl: ActivityResultLauncher<Intent>
 
     //data source
     private val participanteList: MutableList<Participante> = mutableListOf()
@@ -31,10 +38,24 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(amb.root)
+        fillParticipantes()
 
         setSupportActionBar(amb.toolbarIn.toolbar)
         amb.participantelv.adapter= participanteAdapter
-        
+
+        carl = registerForActivityResult(
+            ActivityResultContracts.StartActivityForResult())
+            {result ->
+                if (result.resultCode == RESULT_OK){
+                    val participante =result.data?.getParcelableExtra<Participante>(PARTICIPANTE_EXTRA)
+                    participante?.let{_participante ->
+                        participanteList.add(_participante)
+                        participanteAdapter.add(_participante.nome)
+                        participanteAdapter.notifyDataSetChanged()
+                    }
+                }
+            }
+
     }
 
     override fun onCreateOptionsMenu(menu: Menu?): Boolean {
@@ -45,12 +66,17 @@ class MainActivity : AppCompatActivity() {
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return when(item.itemId){
             R.id.addParticipanteMi -> {
-                //abrir a outra tela
+                carl.launch(Intent(this,ParticipanteActivity::class.java))
                 true
             }
             else -> true
         }
     }
 
+    private fun fillParticipantes(){
+        for (i in 1..5){
+            participanteList.add(Participante(i,"nome$i","produtos", 2.00))
+        }
+    }
 
 }
