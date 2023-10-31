386b1941e865dede3b67f445c48d42ae2fada3c4
AlexandreSCavaleiro <alexandre_cavaleiro@outlook.com>
2023-10-31 00:57:32 -0300

    build: adapter montado, nao testado

---
+++ app/src/main/java/br/edu/scl/ifsp/ads/splitthebill/adapter/ParticipanteAdapter.kt
@@ -0,0 +1,32 @@
+package br.edu.scl.ifsp.ads.splitthebill.adapter
+
+import android.content.Context
+import android.content.Context.LAYOUT_INFLATER_SERVICE
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ArrayAdapter
+import android.widget.TextView
+import androidx.core.content.getSystemService
+import br.edu.scl.ifsp.ads.splitthebill.R
+import br.edu.scl.ifsp.ads.splitthebill.model.Participante
+
+class ParticipanteAdapter(context: Context, private val parList: MutableList<Participante>
+): ArrayAdapter<Participante>(context, R.layout.tile_participante, parList)  {
+
+    override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
+        val participante = parList[position]
+
+        var participanteTileView = convertView
+        if(participanteTileView == null){
+            participanteTileView = (context.getSystemService(LAYOUT_INFLATER_SERVICE) as
+                    LayoutInflater).inflate(R.layout.tile_participante, parent, false)
+        }
+
+        participanteTileView!!.findViewById<TextView>(R.id.nomeTV).setText(participante.nome)
+        participanteTileView!!.findViewById<TextView>(R.id.valorTGTV).setText(participante.valorGasto.toString())
+
+
+        return participanteTileView
+    }
+}
--- app/src/main/java/br/edu/scl/ifsp/ads/splitthebill/view/ParticipanteActivity.kt
+++ app/src/main/java/br/edu/scl/ifsp/ads/splitthebill/view/ParticipanteActivity.kt
@@ -1,8 +1,12 @@
 package br.edu.scl.ifsp.ads.splitthebill.view
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import br.edu.scl.ifsp.ads.splitthebill.databinding.ActivityParticipanteBinding
+import br.edu.scl.ifsp.ads.splitthebill.model.Constants.PARTICIPANTE_EXTRA
+import br.edu.scl.ifsp.ads.splitthebill.model.Participante
+import kotlin.random.Random
 
 
 class ParticipanteActivity: AppCompatActivity() {
@@ -14,8 +18,24 @@ class ParticipanteActivity: AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(apb.root)
 
-        setSupportActionBar(apb.toolbarIn.toolbar)
+        with (apb){
+            salvarBt.setOnClickListener{
+                val participante: Participante = Participante(
+                    id = generateId(),
+                    nome = nomeEt.text.toString(),
+                    valorGasto = valorGastoEt.text.toString().toDouble(),
+                    itensComprados = itemsCompradosEt.text.toString()
+                )
 
+                val resultIntent = Intent()
+                resultIntent.putExtra(PARTICIPANTE_EXTRA, participante)
+                setResult(RESULT_OK, resultIntent)
+                finish()
+            }
+        }
+
+        setSupportActionBar(apb.toolbarIn.toolbar)
     }
 
+    private fun generateId(): Int = Random(System.currentTimeMillis()).nextInt()
 }
